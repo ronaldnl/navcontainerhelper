7e78eef0a9ddc6ed2f5e24be6eb7d2fe82dba992
ronaldnl <74051771+ronaldnl@users.noreply.github.com>
2024-01-15 11:11:10 +0100

    Create Get-BcEnvironmentAvailableAppUpdates.ps1
    Added wrapper for AppSource app updates to be able to check if we are on latest version.

---
+++ Saas/Get-BcEnvironmentAvailableAppUpdates.ps1
@@ -0,0 +1,54 @@
+<#
+ .Synopsis
+  Function for retrieving a list of all environments or one selected environment from an online Business Central tenant
+ .Description
+  Function for retrieving a list of all environments or one selected environment from an online Business Central tenant
+  Wrapper for https://learn.microsoft.com/en-us/dynamics365/business-central/dev-itpro/administration/administration-center-api_environments#get-environment-by-application-family-and-name
+ .Parameter bcAuthContext
+  Authorization Context created by New-BcAuthContext.
+ .Parameter applicationFamily
+  Application Family in which the environment is located. Default is BusinessCentral.
+ .Parameter environment
+  Name of the environment
+ .Parameter apiVersion
+  API version. Default is v2.15.
+ .Example
+  $authContext = New-BcAuthContext -includeDeviceLogin
+  Get-BcEnvironmentAvailableAppUpdates -bcAuthContext $authContext -environment "MySandbox"
+#>
+function Get-BcEnvironmentAvailableAppUpdates {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [Hashtable] $bcAuthContext,
+        [string] $applicationFamily = "BusinessCentral",
+        [string] $environment = "",
+        [string] $apiVersion = "v2.15"
+    )
+
+    $telemetryScope = InitTelemetryScope -name $MyInvocation.InvocationName -parameterValues $PSBoundParameters -includeParameters @()
+    try {
+    
+        $bcAuthContext, $headers, $endPointURL = Create-SaasUrl -bcAuthContext $bcAuthContext -environment $environment -applicationFamily $applicationFamily -apiVersion $apiVersion -endpoint 'apps/availableUpdates'
+        try {
+            $Result = (Invoke-RestMethod -Method Get -UseBasicParsing -Uri $endPointURL -Headers $headers)
+            if ($Result.PSObject.Properties.Name -eq 'Value') {
+                $Result.Value
+            }
+            else {
+                $Result
+            }
+        }
+        catch {
+            throw (GetExtendedErrorMessage $_)
+        }
+    }
+    catch {
+        TrackException -telemetryScope $telemetryScope -errorRecord $_
+        throw
+    }
+    finally {
+        TrackTrace -telemetryScope $telemetryScope
+    }
+}
+Set-Alias -Name Get-BcAppSourceUpdates -Value Get-BcEnvironmentInstalledExtensions
+Export-ModuleMember -Function Get-BcEnvironmentAvailableAppUpdates -Alias Get-BcAppSourceUpdates
